735735dad00f44c2aade3fba56fe54c802d6aa35
Lukas Winter <webmaster@geloescht.net>
2025-01-15 23:39:12 +0200

    Add support for thumb mode plus minor code cleanup

--- src/trampolines/armv7/armv7.c
+++ src/trampolines/armv7/armv7.c
@@ -31,27 +31,50 @@ struct SymbolData *pivotSymbol(const char *symbol, void *newaddr) {
         return NULL;
     }
 
+    int is_thumb_func = (uintptr_t)symboladdr & 1;
+
     struct SymbolData *s = malloc(sizeof(struct SymbolData));
 
     uint32_t addr2 = (((unsigned int) newaddr) >> 16) & 0xFFFF;
     uint32_t addr1 = (((unsigned int) newaddr) >> 0) & 0xFFFF;
 
-    instr_t trampoline[] = {
-        0xE300C000 | (addr1 & 0xFFF) | ((addr1 & 0xF000) << 4), // MOVW R12
-        0xE340C000 | (addr2 & 0xFFF) | ((addr2 & 0xF000) << 4), // MOVT R12
-        0xE12FFF1C  // BX R12
-    };
-    
+    instr_t trampoline[3];
+
+    if(!is_thumb_func) {
+        memcpy(trampoline, (instr_t[]){
+            0xE300C000 | (addr1 & 0xFFF) | ((addr1 & 0xF000) << 4), // MOVW R12
+            0xE340C000 | (addr2 & 0xFFF) | ((addr2 & 0xF000) << 4), // MOVT R12
+            0xE12FFF1C  // BX R12
+        }, 3 * sizeof(instr_t));
+    } else {
+        memcpy(trampoline, (instr_t[]){
+            0xF8DFC008, // LDR R12, [PC, #8]
+            0x4760BF00, // BX R12; NOP
+            (instr_t)newaddr
+        }, 3 * sizeof(instr_t));
+    }
+
     uint32_t addr2f = (((unsigned int) s) >> 16) & 0xFFFF;
     uint32_t addr1f = (((unsigned int) s) >> 0) & 0xFFFF;
 
-    instr_t s2trampoline[] = {
-        0xE300C000 | (addr1f & 0xFFF) | ((addr1f & 0xF000) << 4), // MOVW R12, s[0-15]
-        0xE340C000 | (addr2f & 0xFFF) | ((addr2f & 0xF000) << 4), // MOVT R12, s[16-31]
+    instr_t s2trampoline[4];
 
-        0xe51ff004, // LDR PC, [PC, #-4] 
-        (instr_t)untrampolineStep2 // address loaded by previous instruction, never executed
-    };
+    if(!is_thumb_func) {
+        memcpy(s2trampoline, (instr_t[]){
+            0xE300C000 | (addr1f & 0xFFF) | ((addr1f & 0xF000) << 4), // MOVW R12, s[0-15]
+            0xE340C000 | (addr2f & 0xFFF) | ((addr2f & 0xF000) << 4), // MOVT R12, s[16-31]
+
+            0xe51ff004, // LDR PC, [PC, #-4]
+            (instr_t)untrampolineStep2 // address loaded by previous instruction, never executed
+        }, 4 * sizeof(instr_t));
+    } else {
+        memcpy(s2trampoline, (instr_t[]){
+            0xf8dfc00c, // LDR R12, [PC, #12]
+            0xf8dff00c, // LDR PC,  [PC, #12]
+            (instr_t)untrampolineStep2,
+            (instr_t)s // addresses loaded by previous instructions, never executed
+        }, 4 * sizeof(instr_t));
+    }
 
     // During the restore-call, there will be 2 trampolines at the start of the function.
     uint8_t *funcstart = malloc(ARCHDEP_TRAMPOLINE_LENGTH + ARCHDEP_UNTRAMPOLINE_LENGTH);
