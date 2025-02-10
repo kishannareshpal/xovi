c69fd32a1b0baca8e7370ee09a2a7659653b7edf
Lukas Winter <webmaster@geloescht.net>
2025-02-10 12:03:03 +0200

    Implement adjustment of trampoline2 offset to Thumb-2 instruction boundaries

--- src/trampolines/aarch64/aarch64.h
+++ src/trampolines/aarch64/aarch64.h
@@ -1,5 +1,6 @@
 #pragma once
 #define ARCHDEP_UNTRAMPOLINE_LENGTH (9 * 4)
 #define ARCHDEP_TRAMPOLINE_LENGTH (5 * 4)
+#define ARCHDEP_SYMBOLDATA_FIELDS
 typedef unsigned int instr_t;
 typedef unsigned long long int ptrint_t;
