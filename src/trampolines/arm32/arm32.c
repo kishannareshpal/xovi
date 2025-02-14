f20452216f6092c3061c0a218773561525dd5eb3
asivery <asivery@protonmail.com>
2025-02-14 03:50:33 +0100

    Revert "Update the crash-condition for arm32"
    This reverts commit 0b300f2e6b7383b9de8377dea559fc9a11d69c19.

--- src/trampolines/arm32/arm32.c
+++ src/trampolines/arm32/arm32.c
@@ -105,8 +105,12 @@ extern void untrampolineStackShift(void);
 void generateUntrampoline(void *function, struct SymbolData *symbol, int bytesRemaining) {
 
     void *untrampoline = untrampolineFunction;
-    if(symbol->argsize > 16) {
+    if(symbol->argsize > 4) {
         untrampoline = untrampolineStackShift;
+    }
+    else if(symbol->argsize > 16) {
+        printf("[F]: Fatal error - cannot hook function with argument size above 64 byte!\n");
+        exit(1);
     }
 
     instr_t trampoline[] = {
